b9da200dfbe34db887165cb2f20e26906883a6f2
rohangulage <147734954+rohangulage@users.noreply.github.com>
2024-09-20 15:00:54 +0530

    Add files via upload

---
+++ valid_paranthesis.py
@@ -0,0 +1,16 @@
+def isValid(s: str) -> bool:
+    stack = []
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in bracket_map:
+            top_element = stack.pop() if stack else '#'
+            if bracket_map[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    
+    return not stack
+
+s = "[{()}]"
+print(isValid(s)) 
