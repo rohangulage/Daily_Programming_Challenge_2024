5057ce26d6eb622959b96ca439021b3cac9102a5
rohangulage <147734954+rohangulage@users.noreply.github.com>
2024-09-28 16:44:10 +0530

    Add files via upload

---
+++ sort_stack.py
@@ -0,0 +1,17 @@
+def sorted_insert(stack, element):
+    if not stack or element > stack[-1]:
+        stack.append(element)
+    else:
+        temp = stack.pop()
+        sorted_insert(stack, element)
+        stack.append(temp)
+
+def sort_stack(stack):
+    if stack:
+        temp = stack.pop()
+        sort_stack(stack)
+        sorted_insert(stack, temp)
+
+stack = [30, -5, 18, 14, -3]
+sort_stack(stack)
+print(stack)  
