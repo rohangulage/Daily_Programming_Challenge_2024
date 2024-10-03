487a1dc88425e0034379a0fdd69a9f34fb5c53e9
rohangulage <147734954+rohangulage@users.noreply.github.com>
2024-10-03 20:48:00 +0530

    Add files via upload

---
+++ tree_node.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def is_valid_bst(root, min_val=float('-inf'), max_val=float('inf')):
+    if not root:
+        return True
+    if not (min_val < root.val < max_val):
+        return False
+    return (is_valid_bst(root.left, min_val, root.val) and
+            is_valid_bst(root.right, root.val, max_val))
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(5)
+
+print(is_valid_bst(root))  
